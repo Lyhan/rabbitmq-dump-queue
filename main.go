7a0a38a18d3b8e4be1a33b0d230dc1569813da2c
Julian Capilla <lyhan_jr@hotmail.com>
2018-04-23 13:27:16 +0200

    Update main.go

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var (
 	insecure_tls = flag.Bool("insecure-tls", false, "Insecure TLS mode: don't check certificates")
 	queue        = flag.String("queue", "", "AMQP queue name")
 	ack          = flag.Bool("ack", false, "Acknowledge messages")
-	maxMessages  = flag.Uint("max-messages", 1000, "Maximum number of messages to dump")
+	maxMessages  = flag.Uint("max-messages", 100000, "Maximum number of messages to dump")
 	outputDir    = flag.String("output-dir", ".", "Directory in which to save the dumped messages")
 	full         = flag.Bool("full", false, "Dump the message, its properties and headers")
 	verbose      = flag.Bool("verbose", false, "Print progress")
